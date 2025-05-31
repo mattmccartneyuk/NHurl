3a5e012aebbccbf9a149b29813840cbfb49cd93e
Matthew McCartney <25149329+mattmccartneyuk@users.noreply.github.com>
2025-05-31 21:59:39 +0100

    Added HurlTest attribute class.

--- src/NHurlSolution/NHurl/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NHurl
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/NHurlSolution/NHurl/HurlTest.cs
@@ -0,0 +1,7 @@
+﻿namespace NHurl;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class HurlTest(string hurlFilePath) : Attribute
+{
+    public string HurlFilePath { get; set; } = hurlFilePath;
+}
